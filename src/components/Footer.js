3573d0ceaf722586357cd26c6cfff331ab80296f
Sakshi Priya <priyasakshi8989@gmail.com>
2025-03-04 18:01:35 +0530

    created footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <div className="journal-footer">
+            <caption> Ingrid G. 2025 | </caption>
+            <caption className="footer-caption"> 🌍✈️ Travel Blog </caption>
+        </div>
+    )
+};
+
+export default Footer;
