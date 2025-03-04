318c43f799da4f45a410f28a21c811afee3c1b25
Sakshi Priya <priyasakshi8989@gmail.com>
2025-03-04 17:37:07 +0530

    Header Completed

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return(
+        <div>
+            <header className="journal-header">
+                <h1 className="title"> My Travel Journal... </h1>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
