00c2b3a8ca9f401b41e40e3bcd11925f06f707f7
Sakshi Priya <priyasakshi8989@gmail.com>
2025-03-16 22:18:54 +0530

    Data added

--- src/Data.js
+++ src/Data.js
@@ -9,5 +9,49 @@ export const data = [
 		state: "Uttrakhand",
 		googleMapLink: 
 "https://www.google.com/maps/place/Kempty+Falls/@30.4878339,78.0341178,17z/data=!3m1!4b1!4m6!3m5!1s0x3908d19963f63b51:0xaf4d748e3bc60a88!8m2!3d30.4878339!4d78.0366927!16s%2Fg%2F1pxxrv21z?entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D",
+	},
+	{
+		id: 2,
+		date: "December 2022",
+		title: "Munnar, Kerala.....",
+		description: 
+"Munnar is a town in the Western Ghats mountain range in India's Kerala state. A hill station and former resort for the British Raj elite, it's surrounded by rolling hills dotted with tea plantations established in the late 19th century. Eravikulam National Park, a habitat for the endangered mountain goat Nilgiri tahr, is home to the Lakkam Waterfalls, hiking trails and 2,695m-tall Anamudi Peak.",
+		coverImg: "./images/Munnar.jpg",
+		state: "Kerala",
+		googleMapLink: 
+"https://www.google.com/maps/place/Munnar,+Kerala+685612/@10.0806473,77.0252088,14z/data=!3m1!4b1!4m6!3m5!1s0x3b0799794d099a6d:0x63250e5553c7e0c!8m2!3d10.0889333!4d77.0595248!16zL20vMDFydmpi?entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D",
+	},
+    {
+		id: 3,
+		date: "August 2024",
+		title: "Gucchupani Robber's Cave, Dehradun.....",
+		description: 
+"Robber's Cave, also known as Guchhupani, is a river cave in Dehradun, Uttarakhand, India. It's a popular tourist destination known for its natural beauty and unique features.",
+		coverImg: "./images/Robbers-Cave.avif",
+		state: "Dehradun",
+		googleMapLink: 
+"https://www.google.com/maps/place/GUCCHUPANI+ROBBERS+CAVE/@30.3753952,78.057282,17z/data=!3m1!4b1!4m6!3m5!1s0x3908d75eb8be4f07:0xc25c085050729e14!8m2!3d30.3753952!4d78.0598569!16s%2Fg%2F11t7_v372z?entry=ttu&g_ep=EgoyMDI1MDMwMi4wIKXMDSoASAFQAw%3D%3D",
+	},
+    {
+		id: 4,
+		date: "January 2025",
+		title: "Victoria Memorial, Kolkata.....",
+		description: 
+"The Victoria Memorial is a large marble monument in the Maidan in Central Kolkata, having its entrance on the Queen's Way. It was built between 1906 and 1921 by the British Raj. It is dedicated to the memory of Queen Victoria, the Empress of India from 1876 to 1901.",
+		coverImg: "./images/Victoria-Memorial.jpg",
+		state: "Kolkata",
+		googleMapLink: 
+"https://www.google.com/maps/place/Victoria+Memorial/@22.5448082,88.3399829,17z/data=!3m1!4b1!4m6!3m5!1s0x3a02771346ae015d:0xb540e4bce39763!8m2!3d22.5448082!4d88.3425578!16zL20vMDM3NTJy?entry=ttu&g_ep=EgoyMDI1MDMwOC4wIKXMDSoASAFQAw%3D%3D",
+	},
+	{
+		id: 5,
+		date: "March 2025",
+		title: "Shri Mahakaleshwar Jyotirlinga Temple Ujjain.....",
+		description: 
+"Mahakaleshwar Jyotirlinga is a Hindu temple dedicated to Shiva and is one of the twelve Jyotirlingas, shrines which are said to be the most sacred abodes of Shiva. It is located in the ancient city of Ujjain in the state of Madhya Pradesh, India. The temple is situated on the side of the holy river Shipra. The presiding deity, Shiva in the lingam form is believed to be Swayambhu, deriving currents of power (Shakti) from within itself as against the other images and lingams that are ritually established and invested with mantra-shakti.",
+		coverImg: "./images/Mahakaleahwar.avif",
+		state: "Madhya Pradesh",
+		googleMapLink: 
+"https://www.google.com/maps/place/Shri+Mahakaleshwar+Jyotirlinga+Temple/@23.1827414,75.7656225,17z/data=!3m1!4b1!4m6!3m5!1s0x3963743638ffffff:0x3024e4d1bd13cc22!8m2!3d23.1828736!4d75.7682321!16zL20vMDZzaDBu?entry=ttu&g_ep=EgoyMDI1MDMxMi4wIKXMDSoASAFQAw%3D%3D",
 	}
 ]
