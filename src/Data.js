fbbe78c556587c5ad7ee6d58a29842e52fddc7c3
Sakshi Priya <priyasakshi8989@gmail.com>
2025-03-16 20:56:50 +0530

    Data and card files are completed

--- src/Data.js
+++ src/Data.js
@@ -2,7 +2,7 @@ export const data = [
 	{
 		id: 1,
 		date: "May 2018",
-		title: "Kempty Falls, Mussoorie...",
+		title: "Kempty Falls, Mussoorie.....",
 		description: 
 "Kempty Falls is a waterfall in Ram Gaon and at the south of Kempty, in the Tehri Garhwal District of Uttarakhand, India.[1] It is 13 kilometres (8 mi) from Mussoorie on the Chakrata Road, and 45 kilometres (28 mi) from Dehradun.[2] It is nearly 1364 meters above sea level, at 78°-02’East longitude and 30° -29’North latitude. The Kempty Falls, and the area around is surrounded by high mountain ranges at an altitude of 4500 feet.[3] There is an estimated tourist inflow to the Falls of over 10 lakh (one million).",
 		coverImg: "./images/Kempty-Falls.jpg",
@@ -13,7 +13,7 @@ export const data = [
 	{
 		id: 2,
 		date: "December 2022",
-		title: "Munnar, Kerala...",
+		title: "Munnar, Kerala.....",
 		description: 
 "Munnar is a town in the Western Ghats mountain range in India's Kerala state. A hill station and former resort for the British Raj elite, it's surrounded by rolling hills dotted with tea plantations established in the late 19th century. Eravikulam National Park, a habitat for the endangered mountain goat Nilgiri tahr, is home to the Lakkam Waterfalls, hiking trails and 2,695m-tall Anamudi Peak.",
 		coverImg: "./images/Munnar.jpg",
@@ -24,7 +24,7 @@ export const data = [
     {
 		id: 3,
 		date: "August 2024",
-		title: "Gucchupani Robber's Cave, Dehradun...",
+		title: "Gucchupani Robber's Cave, Dehradun.....",
 		description: 
 "Robber's Cave, also known as Guchhupani, is a river cave in Dehradun, Uttarakhand, India. It's a popular tourist destination known for its natural beauty and unique features.",
 		coverImg: "./images/Robbers-Cave.avif",
@@ -35,7 +35,7 @@ export const data = [
     {
 		id: 4,
 		date: "January 2025",
-		title: "Victoria Memorial, Kolkata...",
+		title: "Victoria Memorial, Kolkata.....",
 		description: 
 "The Victoria Memorial is a large marble monument in the Maidan in Central Kolkata, having its entrance on the Queen's Way. It was built between 1906 and 1921 by the British Raj. It is dedicated to the memory of Queen Victoria, the Empress of India from 1876 to 1901.",
 		coverImg: "./images/Victoria-Memorial.jpg",
@@ -46,7 +46,7 @@ export const data = [
 	{
 		id: 5,
 		date: "March 2025",
-		title: "Shri Mahakaleshwar Jyotirlinga Temple Ujjain...",
+		title: "Shri Mahakaleshwar Jyotirlinga Temple Ujjain.....",
 		description: 
 "Mahakaleshwar Jyotirlinga is a Hindu temple dedicated to Shiva and is one of the twelve Jyotirlingas, shrines which are said to be the most sacred abodes of Shiva. It is located in the ancient city of Ujjain in the state of Madhya Pradesh, India. The temple is situated on the side of the holy river Shipra. The presiding deity, Shiva in the lingam form is believed to be Swayambhu, deriving currents of power (Shakti) from within itself as against the other images and lingams that are ritually established and invested with mantra-shakti.",
 		coverImg: "./images/Mahakaleahwar.avif",
