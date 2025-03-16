e6e56c88b0723058d1f7a223e14128c8683d0496
Sakshi Priya <priyasakshi8989@gmail.com>
2025-03-16 20:33:29 +0530

    completed data.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import Header from "./components/Header";
-import './App.css';
-import Footer from "./components/Footer";
+import Header from './components/Header.js'
+import Cards from './components/Card.js'
+import { data } from './Data.js'
+import Footer from './components/Footer.js'
+import './App.css'
 
 function App() {
-    return(
-        <div className='App'>
-            <Header />
-            <Footer />
-        </div>
-    )
+	const mappedData = data.map((item) => {
+		return (
+			<Cards key={item.id} item={item} />
+		)
+	})
+	return (
+		<div className="App">
+			<Header />
+			{mappedData}
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Cards(props) {
+	return (
+		<div className='card'>
+			<section className='column-left'>
+				<img className="card-img" src={props.item.coverImg} alt={props.item.title} />
+			</section>
+			<section className='column-right'>
+				<span className='material-symbols-outlined'>location_on </span>
+				<span className="card-country">{props.item.state} </span>
+				<a href={props.item.googleMapLink} target="_blank" rel="noopener noreferrer">
+					View on Google Maps
+				</a>
+				<h3 className="card-country-title">{props.item.title}</h3>
+				<h4 className="card-date">{props.item.date}</h4>
+				<p>{props.item.description}</p>
+				<span>Read more..</span>
+			</section>
+		</div>
+	);
+}
